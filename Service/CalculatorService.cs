f57d61e823c89a9940bc856b4c175574daac07b3
Paul <paul.j.millar.1971@gmail.com>
2016-08-25 13:03:14 +0100

    Initial Commit

---
+++ Service/CalculatorService.cs
@@ -0,0 +1,35 @@
+﻿namespace Apollo.ServiceModel.Sample
+{
+    public class CalculatorService : ICalculator
+    {
+        /// <summary>
+        /// Service responsible for carrying out calculations
+        /// </summary>
+        /// <param name="n1"></param>
+        /// <param name="n2"></param>
+        /// <returns></returns>
+        public double Add(double n1, double n2)
+        {
+            double result = n1 + n2;
+            return result;
+        }
+
+        public double Subtract(double n1, double n2)
+        {
+            double result = n1 - n2;
+            return result;
+        }
+
+        public double Multiply(double n1, double n2)
+        {
+            double result = n1 * n2;
+            return result;
+        }
+
+        public double Divide(double n1, double n2)
+        {
+            double result = n1 / n2;
+            return result;
+        }
+    }
+}
